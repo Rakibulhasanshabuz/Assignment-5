22ae5386938c2cb6ea07cc77206993a55fb9bdc7
Rakib Hasan <rakibhasan010141@gmail.com>
2021-08-21 00:38:06 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById('memory-second-btn').addEventListener('click', function(){
+    const memoryCost = document.getElementById('memory-cost');
+    const memoryCostText = memoryCost.innerText;
+    const memoryCostNumber = parseFloat(memoryCostText);
+    me
+})
